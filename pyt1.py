73013d79e20bb067d2d606c96bf6945d4a77cdea
ZJZ <619802353@qq.com>
2018-07-05 16:09:55 +0800

    add pyt1

---
+++ pyt1.py
@@ -0,0 +1,46 @@
+# print("咬")
+#
+# # 注释
+#
+# a = 100
+# print(a)
+#
+# a = 'zhangsan'
+# print(a)
+#
+# print("my name is %s" %a)
+#
+# name = 'lisi'
+# age = 30
+#
+# print("my age is %d" %age)
+# print("my name is %s,my age is %d" %(name,age))
+#
+# #定义变量
+# a_1=3
+#
+# userLoginName = "zhnagsan"
+
+# name = '傻子'
+# id = '111111'
+# phone = '110'
+# add = '吉林市xxxx'
+# print("my name is %s, \n my id is %s, \n my phone is %s, \n my add is %s" %(name,id,phone,add))
+
+# a=2
+# b=1
+#
+# a,b=2,1
+# a,b=b,a
+#
+# print(a)
+
+# help(print)
+
+# s=input("请输入姓名：")
+# print(s)
+# # print(type(s))
+
+print(20**3)
+print(20//3.0)
+print(20/3)
